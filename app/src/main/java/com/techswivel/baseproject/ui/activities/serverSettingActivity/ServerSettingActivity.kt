16631c4270ec4e27c6498e27d11c5c1ee3581df2
Nasar Iqbal <nasar.iqbal@techSwivel.com>
2021-09-16 20:59:34 +0500

    revert build number setting on server setting screen because current giving null value.

--- app/src/main/java/com/techswivel/baseproject/ui/activities/serverSettingActivity/ServerSettingActivity.kt
+++ app/src/main/java/com/techswivel/baseproject/ui/activities/serverSettingActivity/ServerSettingActivity.kt
@@ -1,5 +1,6 @@
 package com.techswivel.baseproject.ui.activities.serverSettingActivity
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import com.techswivel.baseproject.BuildConfig
@@ -49,7 +50,11 @@ class ServerSettingActivity : BaseActivity() {
 
     private fun getAndSetBuildVersion() {
 
-        val versionCode = BuildConfig.APPCENTER_BUILD_ID
+        val versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            packageManager.getPackageInfo(packageName, 0).longVersionCode
+        } else {
+            packageManager.getPackageInfo(packageName, 0).versionCode
+        }
         val versionName = packageManager
             .getPackageInfo(packageName, 0).versionName
         val buildVersion =
